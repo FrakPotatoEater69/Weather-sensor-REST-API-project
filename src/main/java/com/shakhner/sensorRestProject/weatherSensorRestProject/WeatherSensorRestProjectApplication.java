d1c5d3476bad70854a4c20fee777abef2e1afd0c
Aliaksey Shakhner <shahner.lesha@gmail.com>
2022-10-18 13:20:10 +0300

    Initialize project

---
+++ src/main/java/com/shakhner/sensorRestProject/weatherSensorRestProject/WeatherSensorRestProjectApplication.java
@@ -0,0 +1,13 @@
+package com.shakhner.sensorRestProject.weatherSensorRestProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherSensorRestProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherSensorRestProjectApplication.class, args);
+	}
+
+}
